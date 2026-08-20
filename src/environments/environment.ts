fbd3d02ee4241c600af3a030e9427a0592656a74
Soilihi Mohamed <amirmaoueni@gmail.com>
2026-08-20 17:08:39 +0000

    chore(config): mise a jour du gitignore et configuration des environnements

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://ksdfecurnstwtwlkfwxm.supabase.co',
-  supabaseAnonKey: 'sb_publishable_hd7kyYJC092LtH4BfoDlzw_OX9QA9XS',
-};
